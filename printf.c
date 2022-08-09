1adb5ce54090e0b8b593a5981badc267d22e1090
nokekip <kipnoke@gmail.com>
2022-08-09 05:51:10 -0700

    task 0

--- printf.c
+++ printf.c
@@ -1,86 +1,66 @@
 #include "main.h"
 
-/**
- * clean - Peforms clean operations
- * @params: va_list
- * @output: A buffer_t struct.
- */
-void clean(va_list params, buffer_t *output)
-{
-	va_end(params);
-	write(1, output->start, output->len);
-	free_buffer(output);
-}
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * print - Reads through the format string
- * @format: Character string to print
- * @output: A buffer_t struct containing a buffer.
- * @params: A va_list of arguments.
- *
- * Return: The number of characters
- */
-int print(const char *format, va_list params, buffer_t *output)
-{
-	int i, wid, prec, ret = 0;
-	char tmp;
-	unsigned char flags, len;
-	unsigned int (*f)(va_list, buffer_t *, unsigned char,
-			int, int, unsigned char);
-
-	for (i = 0; *(format + i); i++)
-	{
-		len = 0;
-		if (*(format + i) == '%')
-		{
-			tmp = 0;
-			flags = handle_flags(format + i + 1, &tmp);
-			wid = handle_width(params, format + i + tmp + 1, &tmp);
-			prec = handle_precision(params, format + i + tmp + 1,
-					&tmp);
-			len = handle_length(format + i + tmp + 1, &tmp);
-
-			f = handle_specifiers(format + i + tmp + 1);
-			if (f)
-			{
-				i += tmp + 1;
-				ret += f(params, output, flags, wid, prec, len);
-				continue;
-			}
-			else if (!*(format + i + tmp + 1))
-			{
-				ret = -1;
-				break;
-			}
-		}
-		ret += _memcpy(output, (format + i), 1);
-		i += (len != 0) ? 1 : 0;
-	}
-	clean(params, output);
-	return (ret);
-}
-
-/**
- * _printf - Outputs a formatted string.
- * @format: Character string to print
- *
- * Return: The number of characters
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-	buffer_t *output;
-	va_list params;
-	int ret;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	if (!format)
-		return (-1);
-	output = init_buffer();
-	if (!output)
+	if (format == NULL)
 		return (-1);
 
-	va_start(params, format);
+	va_start(list, format);
 
-	ret = print(format, params, output);
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
 
-	return (ret);
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
